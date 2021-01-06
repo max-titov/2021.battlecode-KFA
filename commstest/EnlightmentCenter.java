65e0a45eca674e3f0a281f97ee9735ef9ea41874
Max Titov <max.teetov@gmail.com>
2021-01-06 12:54:14 -0500

    added test folders

---
+++ commstest/EnlightmentCenter.java
@@ -0,0 +1,23 @@
+package organizedplayer;
+import battlecode.common.*;
+
+public class EnlightmentCenter extends Robot {
+
+    public EnlightmentCenter(RobotController r){
+        super(r);
+    }
+
+    public void takeTurn() throws GameActionException{
+        super.takeTurn();
+        RobotType toBuild = randomSpawnableRobotType();
+        int influence = 50;
+        for (Direction dir : directions) {
+            if (rc.canBuildRobot(toBuild, dir, influence)) {
+                rc.buildRobot(toBuild, dir, influence);
+            } else {
+                break;
+            }
+        }
+    }
+
+}
---
+++ commstest/Slanderer.java
@@ -0,0 +1,16 @@
+package organizedplayer;
+import battlecode.common.*;
+
+public class Slanderer extends Robot {
+
+    public Slanderer(RobotController r){
+        super(r);
+    }
+
+    public void takeTurn() throws GameActionException{
+        super.takeTurn();
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+
+}
