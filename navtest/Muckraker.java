7fe016b1c9c763329a7f9b9aa4cbc27f2de4dcdc
Aryan Sawhney <aryansaw03@gmail.com>
2021-01-07 21:03:23 -0500

    Created calcTurnsOfPath and getBaseCooldown method

--- navtest/Muckraker.java
+++ navtest/Muckraker.java
@@ -4,32 +4,32 @@ import battlecode.common.*;
 
 public class Muckraker extends Robot {
 
-	public Muckraker(RobotController r) {
-		super(r);
+	public Muckraker(RobotController rc) {
+		super(rc);
 	}
 
 	public void takeTurn() throws GameActionException {
 		super.takeTurn();
-		int navCircleLen = nav.navCircle.length;
-		MapLocation tempLoc = rc.getLocation();
-		for (int i = 0; i<navCircleLen;i++){
-			tempLoc.add(nav.navCircle[i]);
-			System.out.println(tempLoc);
-			rc.setIndicatorDot(tempLoc, 255, 255, 255);
-		}
+		// int navCircleLen = nav.navCircle.length;
+		// MapLocation tempLoc = rc.getLocation();
+		// for (int i = 0; i < navCircleLen; i++) {
+		// tempLoc.add(nav.navCircle[i]);
+		// System.out.println(tempLoc);
+		// rc.setIndicatorDot(tempLoc, 255, 255, 255);
+		// }
 
-		Team enemy = rc.getTeam().opponent();
-		int actionRadius = rc.getType().actionRadiusSquared;
-		for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
-			if (robot.type.canBeExposed()) {
-				// It's a slanderer... go get them!
-				if (rc.canExpose(robot.location)) {
-					System.out.println("e x p o s e d");
-					rc.expose(robot.location);
-					return;
-				}
-			}
-		}
+		// Team enemy = rc.getTeam().opponent();
+		// int actionRadius = rc.getType().actionRadiusSquared;
+		// for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+		// if (robot.type.canBeExposed()) {
+		// // It's a slanderer... go get them!
+		// if (rc.canExpose(robot.location)) {
+		// System.out.println("e x p o s e d");
+		// rc.expose(robot.location);
+		// return;
+		// }
+		// }
+		// }
 		if (nav.tryMove(randomDirection()))
 			System.out.println("I moved!");
 	}
