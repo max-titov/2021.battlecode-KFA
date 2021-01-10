c923763282bf99f1ef63397142f0dba8d49aaf46
Aryan Sawhney <aryansaw03@gmail.com>
2021-01-09 23:12:17 -0500

    Added comments to muckraker

--- muckrakertest/Muckraker.java
+++ muckrakertest/Muckraker.java
@@ -3,14 +3,25 @@ package muckrakertest;
 import battlecode.common.*;
 
 public class Muckraker extends Robot {
-
+	/**
+	 * Constants
+	 */
 	public final int REGULAR_MUCKRAKER = 1;
 
+	/**
+	 * Muckraker's attributes
+	 */
 	public int muckrakerType;
 	public Direction heading;
 	public Direction headingToRobot;
 	public boolean robotInRange;
 
+	/**
+	 * Constructor Set to a Regular Muckraker at creation Set its heading based on
+	 * whichever side of EC it is spawned Initialize any other variables
+	 * 
+	 * @param rc
+	 */
 	public Muckraker(RobotController rc) {
 		super(rc);
 		muckrakerType = REGULAR_MUCKRAKER;
@@ -18,6 +29,10 @@ public class Muckraker extends Robot {
 		robotInRange = false;
 	}
 
+	/**
+	 * Muckraker's take turn method Switch statement to execute different actions
+	 * for different types of muckrakers
+	 */
 	public void takeTurn() throws GameActionException {
 		super.takeTurn();
 		// check message to see muckraker type
@@ -28,6 +43,14 @@ public class Muckraker extends Robot {
 		}
 	}
 
+	/**
+	 * Regular Muckraker Detects a robot in its expose radius, if detected, kill the
+	 * one with the highest conviction If none is detected, check for any robots in
+	 * sense radius, if found move towards it Otherwise keep exploring map by moving
+	 * towards heading
+	 * 
+	 * @throws GameActionException
+	 */
 	public void regularMuckraker() throws GameActionException {
 		RobotInfo[] robotsInExpose = rc.senseNearbyRobots(12, opponentTeam);
 		RobotInfo[] robotsInSense = rc.senseNearbyRobots(30, opponentTeam);
@@ -44,6 +67,13 @@ public class Muckraker extends Robot {
 		}
 	}
 
+	/**
+	 * Finds the robot with the max conviction given an array of robots and exposes
+	 * it if it can
+	 * 
+	 * @param robotsInSight
+	 * @throws GameActionException
+	 */
 	public void exposeInExposeRad(RobotInfo[] robotsInSight) throws GameActionException {
 		MapLocation exposeLoc = findMaxConv(robotsInSight);
 		if (rc.canExpose(exposeLoc)) {
@@ -51,6 +81,12 @@ public class Muckraker extends Robot {
 		}
 	}
 
+	/**
+	 * Finds the robot with the most conviction
+	 * 
+	 * @param robots
+	 * @return
+	 */
 	public MapLocation findMaxConv(RobotInfo[] robots) {
 		int indexOfMaxConv = 0;
 		for (int i = 0; i < robots.length; i++) {
